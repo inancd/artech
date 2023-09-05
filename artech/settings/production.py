57d590c060251c887e93103f8827985927c2f9d4
inanc dagyolu <inancdagyolu@inancs-Air.home>
2023-09-05 13:44:21 +0300

    Setting Production v1

--- artech/settings/production.py
+++ artech/settings/production.py
@@ -1,3 +1,3 @@
 from .base import *
 
-ALLOWED_HOSTS = ['143.198.55.51', 'www.artech.com', 'artech.com', 'localhost']
+ALLOWED_HOSTS = ['143.198.55.51', 'www.artechmakine.com', 'artechmakine.com', 'localhost']
