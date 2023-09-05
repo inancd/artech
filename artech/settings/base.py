29a24eda20c61828b20d1fb5c38312c9b68334e3
inanc dagyolu <inancdagyolu@inancs-Air.home>
2023-09-05 15:13:38 +0300

    Base

--- artech/settings/base.py
+++ artech/settings/base.py
@@ -28,7 +28,7 @@ TEMPLATE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG') 
+DEBUG = True
 
 
 # Application definition
