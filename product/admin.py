04bb27338d0c8c74c0f79a4c86830bae34393d27
inanc dagyolu <inancdagyolu@inancs-Air.home>
2023-09-08 18:20:03 +0300

    Image Inline

--- product/admin.py
+++ product/admin.py
@@ -1,19 +1,23 @@
 from django.contrib import admin
-from .models import Product, ProductCategory, ProductImage
+from .models import (Product, ProductCategory, ProductImage)
 from mptt.admin import MPTTModelAdmin
 
 # Register your models here.
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
 
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields= {'slug': ('name', )}
+    inlines = [ProductImageInline]
+    
 
-admin.site.register(Product, ProductAdmin)
 
 class ProductCategoryAdmin(MPTTModelAdmin):
     prepopulated_fields= {'slug': ('name', )}
 
 admin.site.register(ProductCategory, ProductCategoryAdmin)
 
-admin.site.register(ProductImage)
+
 
